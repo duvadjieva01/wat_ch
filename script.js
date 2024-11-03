66f2306cdf14073dbbca65ef424a064c3c2739f4
duvadjieva01 <lenovo04042024@gmail.com>
2024-11-03 12:58:42 +0500

    okay

---
+++ script.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].classList.remove("fade");  // remove fade effect
+    }
+    slides[slideIndex].style.display = "block";
+    slides[slideIndex].classList.add("fade"); // add fade effect
+}
+
+// Start the first slide
+showSlides(slideIndex);
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
